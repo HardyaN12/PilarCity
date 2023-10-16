2f2381c144fe72482be8798fd7990bd85cd976e2
HardyaN12 <hardian.gow@gmail.com>
2023-10-15 22:48:43 -0600

    v1.1

---
+++ app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    // Función para activar animaciones en los elementos visibles en la pantalla
+    function activateAnimations() {
+      $(".animate-section").each(function() {
+        var elementTop = $(this).offset().top;
+        var elementHeight = $(this).outerHeight();
+        var windowHeight = $(window).height();
+        var scroll = $(window).scrollTop();
+        
+        if (scroll > elementTop - windowHeight + elementHeight / 2) {
+          $(this).addClass("active");
+          $(this).find(".animate-on-scroll").each(function() {
+            $(this).addClass("animate__animated").addClass($(this).data("animation"));
+          });
+        }
+      });
+    }
+  
+    // Llama a la función al cargar la página y en eventos de desplazamiento
+    activateAnimations();
+    $(window).scroll(activateAnimations);
+  });
